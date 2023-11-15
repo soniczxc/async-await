9d7903c43a31064e17ea4272018bd7bff3665f8c
soniczxc <139926954+soniczxc@users.noreply.github.com>
2023-11-15 18:16:38 +0300

    Update async_reader.py

--- file_reader/async_reader.py
+++ file_reader/async_reader.py
@@ -2,13 +2,26 @@ import aiofiles
 import asyncio
 import datetime
 
+
 async def read_file(filename):
-    pass # to-do
+    async with aiofiles.open(filename, mode='rb') as f:
+        print(filename)
+        while True:
+            await asyncio.sleep(0.001)
+            data = await f.read(1024)
+            if not data:
+                print(filename, "done ###############")
+                break
+
 
 async def main():
     begin = datetime.datetime.now()
-    files = ['/home/sia/tmp/{}'.format(i) for i in range(1, 11)]
-    #to-do use asyncio.as_completed to call read_file
+    files = ['../files/file{}.bin'.format(i) for i in range(1, 11)]
+    tasks = [read_file(file) for file in files]
+    task_results = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        task_results.append(result)
 
     print(datetime.datetime.now() - begin)
 
